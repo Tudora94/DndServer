1c8891611fd3981b264ca10736c48aae02668b83
Cieran Priest <Cieran-p@live.co.uk>
2023-03-27 21:29:19 +0100

    Added in folder for Classes Added in some names & get requests

--- DndServer/Campaign/Campaign.cs
+++ DndServer/Campaign/Campaign.cs
@@ -3,8 +3,8 @@
     public class Campaign
     {
         private int campaignID = 1;
-        private string campaignName;
+        private string campaignName = "Test";
         public int CampaignID { get => campaignID; set => campaignID = value; }
-        //public string CampaignName { get => campaignName; set => campaignName = value; }
+        public string CampaignName { get => campaignName; set => campaignName = value; }
     }
 }
