97d084066d0de0a7e062f7cfb585499df9048260
Cieran Priest <Cieran-p@live.co.uk>
2023-03-28 19:46:10 +0100

    Added new folder for character specific classes. Created the classes correctly for Campaign & Races. Created the class's Ability Scores and Classes

--- DndServer/Campaign/Campaign.cs
+++ DndServer/Campaign/Campaign.cs
@@ -4,7 +4,14 @@
     {
         private int campaignID = 1;
         private string campaignName = "Test";
+        private int players;
+        private string campaignPreferences;
+        private string advancementType;
+        private string hpType;
+        private string weightType;
+
         public int CampaignID { get => campaignID; set => campaignID = value; }
         public string CampaignName { get => campaignName; set => campaignName = value; }
+
     }
 }
