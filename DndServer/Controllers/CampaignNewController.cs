b334aa46f3a5785d65fa9a04c328c09404e03caa
alex tudor <alextudor@paypoint.com>
2023-04-08 23:53:29 +0100

    CampaignController Added

---
+++ DndServer/Controllers/CampaignNewController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using DndServer.Campaign.Models;
+using DndServer.Dal;
+
+
+namespace DndServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampaignNewController : ControllerBase
+    {
+        [HttpPost("CreateCampaign")]
+        [Authorize]
+        public async Task<ActionResult<int>> createCampaign(CreateCampaignModel request)
+        {
+            CampaignSql campaignSql = new CampaignSql();
+
+            int CampaignId = campaignSql.CreateCampaign(request);
+
+            request.CampaignId = CampaignId;
+
+            return Ok(request);
+        }
+    }
+}
---
+++ DndServer/Dal/CampaignSql.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using DndServer.User.Models;
+using DndServer.Campaign.Models;
+
+
+
+namespace DndServer.Dal
+
+{
+    public class CampaignSql
+    {
+        private SqlConnection conn = new SqlConnection();
+        ConnectionsSql connections = new ConnectionsSql();
+
+        public int CreateCampaign(CreateCampaignModel model)
+        {
+            try
+            {
+                connections.SqlOpenConnection(conn);
+
+                string SetCampaignId = @"INSERT INTO DndDb.dbo.CampaignName VALUES ((SELECT Id FROM DndDb.dbo.Users WHERE username = @username), @CampaignName)";
+                SqlCommand cmdSetCampaignId = new SqlCommand(SetCampaignId, conn);
+
+                cmdSetCampaignId.Parameters.Add("@username", SqlDbType.VarChar).Value = model.UserName1;
+                cmdSetCampaignId.Parameters.Add("@CampaignName", SqlDbType.VarChar).Value = model.Name;
+
+                cmdSetCampaignId.ExecuteNonQuery();
+
+                string getCampaignId = @"SELECT Id FROM DndDb.dbo.CampaignName WHERE USerId = (SELECT Id FROM DndDb.dbo.Users WHERE username = @username) AND CampaignName = @CampaignName";
+                SqlCommand cmdGetCampaignId = new SqlCommand(getCampaignId, conn);
+
+                cmdGetCampaignId.Parameters.Add("@username", SqlDbType.VarChar).Value = model.UserName1;
+                cmdGetCampaignId.Parameters.Add("@CampaignName", SqlDbType.VarChar).Value = model.Name;
+
+                DataTable dt = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmdGetCampaignId);
+                da.Fill(dt);
+                connections.SQLCloseConnection(conn);
+
+                int CampaignId = 0;
+
+                foreach(DataRow dr in dt.Rows)
+                {
+                    CampaignId = Convert.ToInt32(dr[0]);
+                }
+
+                return CampaignId;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+    }
+}
