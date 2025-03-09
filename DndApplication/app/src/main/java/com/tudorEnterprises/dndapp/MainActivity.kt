eafe287ddd3d8e8b701f5ca59cbc2c032c8956c4
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-09 16:48:38 +0000

    New login page working

---
+++ DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tudorEnterprises.dndapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tudorEnterprises.dndapp.navigation.NavigationController
+import com.tudorEnterprises.dndapp.ui.theme.DndApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationController()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DndApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/constants/AppConstants.kt
@@ -0,0 +1,4 @@
+package com.tudorEnterprises.dndapp.constants
+
+const val appName = "D&D App"
+const val baseUrl = "https://www.tudor-enterprises.com"
