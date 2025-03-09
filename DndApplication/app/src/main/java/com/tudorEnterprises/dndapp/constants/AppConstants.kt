f8c99b526f72901df9986d0b98dbb70513d6a030
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-09 16:44:22 +0000

    Removed unNeeded files

--- DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/constants/AppConstants.kt
+++
@@ -1,4 +0,0 @@
-package com.tudorEnterprises.dndapp.constants
-
-const val appName = "D&D App"
-const val baseUrl = "https://www.tudor-enterprises.com"
--- DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/constants/Enums.kt
+++
@@ -1,5 +0,0 @@
-package com.tudorEnterprises.dndapp.constants
-
-enum class Screen(val route: String) {
-    Home("homeScreen"),
-}
