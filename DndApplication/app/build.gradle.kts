5c213e3c9923a7fc302e6aade3a2c6bc043de505
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-14 00:25:55 +0000

    Updated version number

--- DndApplication/app/build.gradle.kts
+++ DndApplication/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.dndapplication"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.1"
+        versionCode = 2
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
