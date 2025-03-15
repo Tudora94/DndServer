1bcb9507206a66bbec0ec45a9f2e414b178ff99b
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-15 12:33:40 +0000

    Live UPDATES and removed need to send localDb ID, as primary key is the ID passed by the server

--- DndServer/Campaign/Models/CreateCampaignRequestModel.cs
+++ DndServer/Campaign/Models/CreateCampaignRequestModel.cs
@@ -1,12 +1,10 @@
 ﻿namespace DndServer.Campaign.Models
 {
-    public class CreateCampaignModel
+    public class CreateCampaignRequestModel
     {
-        private int _campaignId;
         private int _userId;
         private string _name = "";
 
-        public int CampaignId { get => _campaignId; set => _campaignId = value; }
         public int UserId { get => _userId; set => _userId = value; }
         public string Name { get => _name; set => _name = value; }
     }
---
+++ DndServer/Campaign/Models/CreateCampaignResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DndServer.Campaign.Models
+{
+    public class CreateCampaignResponseModel : CreateCampaignRequestModel
+    {
+        private int _campaignId;
+
+        public int CampaignId { get => _campaignId; set => _campaignId = value; }
+    }
+}
