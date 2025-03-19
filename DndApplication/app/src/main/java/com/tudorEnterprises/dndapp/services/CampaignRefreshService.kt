baa054403b52138e9cc157f5c323c407c1c8f3ea
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-19 21:20:40 +0000

    Added delete via refresh

--- DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/services/CampaignRefreshService.kt
+++ DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/services/CampaignRefreshService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import com.tudorEnterprises.dndapp.dataStorage.CampaignSqlActivity
 import com.tudorEnterprises.dndapp.networking.CampaignHttp
+import kotlinx.coroutines.flow.first
 
 class CampaignRefreshService(val context: Context, val dbCalls: CampaignSqlActivity) {
 
@@ -13,7 +14,21 @@ class CampaignRefreshService(val context: Context, val dbCalls: CampaignSqlActiv
         Log.d("refresh Service", "refresh service called")
         val campaignList = httpCalls.getCampaigns()
         if (campaignList != null) {
-            for(campaign in campaignList) {
+            val localCampaigns = dbCalls.getAllCampaigns()
+            val serverIds = campaignList.map { it.campaignId }.toSet()
+
+            val campaignsToDelete =
+                localCampaigns.first().filter { it.syncCampaignId !in serverIds }
+            if (campaignsToDelete.isNotEmpty()) {
+                for (campaign in campaignsToDelete) {
+                    Log.d("campaign Deletion", "deleting campaign ${campaign.syncCampaignId}")
+                    dbCalls.deleteCampaignById(campaign.syncCampaignId)
+                }
+            }
+
+            for (campaign in campaignList) {
+
+                //TODO api and local change needed to include last Updated Date as epoch time
                 dbCalls.upsertCampaign(campaign)
             }
         }
