05b76c3a62765c098616831ceb2a8669dbeacaee
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-03-31 11:55:30 +0100

    Updated SQL to include UpdateTime and no longer need campaign code

--- SQL/AddPlayerNameSpo.sql
+++ SQL/AddPlayerNameSpo.sql
@@ -1,23 +1,18 @@
 USE DndDb
 GO
 CREATE PROCEDURE AddNewPlayer
-@roomCode VARCHAR(255),
 @name VARCHAR(255),
-@userName VARCHAR(255)
+@userId INT,
+@updateTime BIGINT
 AS
 BEGIN
-
-DECLARE @userId INT
-SET @userId = (SELECT TOP(1) Id FROM Users WHERE username = @userName)
-
-DECLARE @CampaignId INT
-SET @CampaignId = (SELECT TOP(1) CampaignId FROM CampaignRoomCode WHERE CampaignCode = @roomCode)
 
 INSERT INTO PlayerCharacterName
 VALUES
 (
 @userId,
-@CampaignId,
-@name
+0,
+@name,
+@updateTime
 )
 END
