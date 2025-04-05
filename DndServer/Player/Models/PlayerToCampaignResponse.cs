e8225d0b110d00e423fbdd9cdc2bde0dcec287ef
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-04-05 11:01:10 +0100

    Updated add player response

--- DndServer/Player/Models/PlayerToCampaignResponse.cs
+++ DndServer/Player/Models/PlayerToCampaignResponse.cs
@@ -4,8 +4,10 @@
     {
         private bool _success = false;
         private int? _campaignId;
+        private string? _campaignName;
 
         public bool Success { get => _success; set => _success = value; }
         public int? CampaignId { get => _campaignId; set => _campaignId = value; }
+        public string CampaignName { get => _campaignName; set => _campaignName = value; }
     }
 }
