ecd87dc2404504d53c444de3d1f7a4b63f06f583
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-07-11 18:47:24 +0100

    Added create inventory item

---
+++ DndServer/Controllers/InventoryController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using DndServer.Dal;
+using DndServer.User.Services;
+using DndServer.Inventory.Models;
+using DndServer.Inventory.Services;
+
+namespace DndServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        AuthSql authSql = new AuthSql();
+        ClaimValidator claimValidator = new ClaimValidator();
+        InventoryService inventoryService = new InventoryService();
+
+        private readonly ILogger<PlayerController> _logger;
+
+        public InventoryController(ILogger<PlayerController> logger)
+        {
+            {
+                _logger = logger;
+            }
+        }
+
+        [HttpPost("CreateInventoryItem")]
+        public async Task<ActionResult<CreateInventoryItemResponse>> CreateInventoryItem(CreateInventoryItemRequest request)
+        {
+            var token = HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            var claimAccepted = claimValidator.validateClaimUser(request.UserId, token, authSql);
+
+            var response = new CreateInventoryItemResponse();
+
+            if (claimAccepted)
+            {
+                var itemId = inventoryService.CreateInventoryItemAndGetId(request);
+
+                response.ItemId = itemId;
+                response.Message = "item added successfully";
+                response.Success = true;
+
+                //TODO handle failure to add
+
+                return Ok(response);
+            }
+            else
+            {
+                response.Message = "Invalid user";
+                return BadRequest(response); //TODO return a better response than a string.
+            }
+        }
+
+    }
+}
---
+++ DndServer/Dal/InventorySql.cs
@@ -0,0 +1,42 @@
+﻿using DndServer.Inventory.Models;
+using System.Data.SqlClient;
+
+namespace DndServer.Dal
+{
+    public class InventorySql
+    {
+        ConnectionsSql connections = new ConnectionsSql();
+
+        public int CreateInventoryItem(CreateInventoryItemRequest request)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection();
+                connections.SqlOpenConnection(conn);
+
+                string setInventoryItemId = @"INSERT INTO DndDb.dbo.Inventory
+    OUTPUT INSERTED.Id
+    VALUES(@userId, null, null, @itemName, @itemDescription, @itemDetail, @updateTime)";
+
+                SqlCommand cmd = new SqlCommand(setInventoryItemId, conn);
+
+                cmd.Parameters.Add("@userId", System.Data.SqlDbType.Int).Value = request.UserId;
+                cmd.Parameters.Add("@itemName", System.Data.SqlDbType.VarChar).Value = request.ItemName;
+                cmd.Parameters.Add("@itemDescription", System.Data.SqlDbType.VarChar).Value = request.ItemDescription;
+                cmd.Parameters.Add("@itemDetail", System.Data.SqlDbType.VarChar).Value = request.ItemDetail;
+                cmd.Parameters.Add("@updateTime", System.Data.SqlDbType.BigInt).Value = request.UpdateTime;
+
+                int ItemId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                connections.SQLCloseConnection(conn);
+
+                return ItemId;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+
+        }
+    }
+}
