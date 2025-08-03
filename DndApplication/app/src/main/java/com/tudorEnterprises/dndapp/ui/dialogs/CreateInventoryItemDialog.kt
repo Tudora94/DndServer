4454d53243d53e64e407673a2754648da08d655c
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-08-03 21:18:47 +0100

    Updated create item dialog

--- DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/ui/dialogs/CreateInventoryItemDialog.kt
+++ DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/ui/dialogs/CreateInventoryItemDialog.kt
@@ -1,7 +1,10 @@
 package com.tudorEnterprises.dndapp.ui.dialogs
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedTextField
@@ -11,9 +14,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tudorEnterprises.dndapp.objects.InventoryItem
 
 @Composable
@@ -28,41 +33,65 @@ fun CreateInventoryItemDialog(
     AlertDialog(
         onDismissRequest = { onDismiss() },
         confirmButton = {
-            ElevatedButton(onClick = { onConfirm(inventoryItem)
+            ElevatedButton(onClick = {
+                onConfirm(inventoryItem)
                 keyboardController?.hide()
                 focusManager.clearFocus()
-                onDismiss()}
+                onDismiss()
+            }
             ) {
                 Text("Create")
             }
         },
-        title = {Text("New inventory item")},
+        title = { Text("New inventory item") },
         text = {
-            Column(verticalArrangement = Arrangement.Center) {
-                OutlinedTextField(
-                    value = inventoryItem.name,
-                    onValueChange = { inventoryItem = inventoryItem.copy(name = it) },
-                    label = { Text("Item Name") }
-                )
-
-                OutlinedTextField(
-                    value = inventoryItem.description,
-                    onValueChange = { inventoryItem = inventoryItem.copy(description = it) },
-                    label = { Text("Item Description") }
-                )
-
-                OutlinedTextField(
-                    value = inventoryItem.detail,
-                    onValueChange = { inventoryItem = inventoryItem.copy(detail = it) },
-                    label = { Text("Item Details") }
-                )
-
+            Box(modifier = Modifier.heightIn(max = 300.dp)) { // Limit height to enable scrolling
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    item {
+                        OutlinedTextField(
+                            value = inventoryItem.name,
+                            onValueChange = {
+                                if (it.length <= 255) {
+                                    inventoryItem = inventoryItem.copy(name = it)
+                                }
+                            },
+                            label = { Text("Item Name") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    item {
+                        OutlinedTextField(
+                            value = inventoryItem.description,
+                            onValueChange = {
+                                if (it.length <= 255) {
+                                    inventoryItem = inventoryItem.copy(description = it)
+                                }
+                            },
+                            label = { Text("Item Description") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                    item {
+                        OutlinedTextField(
+                            value = inventoryItem.detail,
+                            onValueChange = { inventoryItem = inventoryItem.copy(detail = it) },
+                            label = { Text("Item Details") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
             }
+
         },
         dismissButton = {
-            ElevatedButton(onClick = { keyboardController?.hide()
+            ElevatedButton(onClick = {
+                keyboardController?.hide()
                 focusManager.clearFocus()
-                onDismiss()}
+                onDismiss()
+            }
             ) {
                 Text("Cancel")
             }
