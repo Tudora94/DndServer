52588fb83805018bebaa6f225f57273db9a90569
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-08-06 16:06:11 +0100

    updated join to return nulls, currently new characters will delete themeselves off the UI

--- DndServer/Dal/PlayerSql.cs
+++ DndServer/Dal/PlayerSql.cs
@@ -116,7 +116,7 @@ namespace DndServer.Dal
             {
                 connections.SqlOpenConnection(conn);
 
-                string sqlString = @"SELECT PCN.ID, PCN.UserId, CampaignId, CharacterName, PCN.UpdateTime, CampaignName FROM DndDb.dbo.PlayerCharacterName AS PCN JOIN DndDb.dbo.CampaignName AS CN ON PCN.CampaignId = CN.Id WHERE PCN.UserId = @userId";
+                string sqlString = @"SELECT PCN.ID, PCN.UserId, CampaignId, CharacterName, PCN.UpdateTime, CampaignName FROM DndDb.dbo.PlayerCharacterName AS PCN LEFT JOIN DndDb.dbo.CampaignName AS CN ON PCN.CampaignId = CN.Id WHERE PCN.UserId = @userId";
                 SqlCommand command = new SqlCommand(sqlString, conn);
 
                 command.Parameters.Add("userId", SqlDbType.Int).Value=UserId;
