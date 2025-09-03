df763ad4abf4583e0b7ded8607cdb62d7b275bd8
Tudora94 <91989204+Tudora94@users.noreply.github.com>
2025-09-03 16:49:30 +0100

    Updating baseUrl

--- DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/constants/AppConstants.kt
+++ DndApplication/app/src/main/java/com/tudorEnterprises/dndapp/constants/AppConstants.kt
@@ -1,4 +1,4 @@
 package com.tudorEnterprises.dndapp.constants
 
-const val baseUrl = "https://www.tudor-enterprises.com/"
+const val baseUrl = "https://www.tudor-enterprises.com/DndServer/"
 //const val baseUrl = "https://localhost:7099/"
